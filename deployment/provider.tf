b879a0076782e262af492233aaf0237032d6660e
Ficox <phico007@gmail.com>
2025-10-04 17:19:19 +0200

    Add helm files and terraform

---
+++ deployment/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.32" }
+    helm       = { source = "hashicorp/helm",       version = "~> 2.13" }
+  }
+  backend "local" {
+    path = "test/terraform.tfstate"
+  }  
+}
+
+
+
+provider "kubernetes" {
+  config_path = "C:\\Users\\Windows 10\\.kube\\config" 
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "C:\\Users\\Windows 10\\.kube\\config"
+  }
+}
